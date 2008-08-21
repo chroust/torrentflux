7d40aa6b0f98ce0585f8c1dc483094c502bc225c
shindasingh <shindasingh@c2936105-3e4e-0410-ac7b-c33532cc5c62>
2008-08-21 00:05:57 +0000

    Updated lastrss, fixing bug with some rss feeds

--- trunk/html/lastRSS.php
+++ trunk/html/lastRSS.php
@@ -1,18 +1,15 @@
 <?php
 /*
  ======================================================================
- lastRSS 0.6
-
- Simple yet powerful PHP class to parse RSS files.
-
- by Vojtech Semecky, webmaster@webdot.cz
-
+ lastRSS 0.9.1
+ 
+ Simple yet powerfull PHP class to parse RSS files.
+ 
+ by Vojtech Semecky, webmaster @ oslab . net
+ 
  Latest version, features, manual and examples:
-     http://lastrss.webdot.cz/
+     http://lastrss.oslab.net/
 
- ----------------------------------------------------------------------
- TODO
- - Iconv nedavat na cely, ale jen na TITLE a DESCRIPTION (u item i celkove)
  ----------------------------------------------------------------------
  LICENSE
 
@@ -30,19 +27,28 @@
  ======================================================================
 */
 
+/**
+* lastRSS
+* Simple yet powerfull PHP class to parse RSS files.
+*/
 class lastRSS {
     // -------------------------------------------------------------------
-    // Settings
+    // Public properties
     // -------------------------------------------------------------------
-    var $channeltags = array ('title', 'link', 'description', 'language', 'copyright', 'managingEditor', 'webMaster', 'pubDate', 'lastBuildDate', 'rating', 'docs');
+    var $default_cp = 'UTF-8';
+    var $CDATA = 'nochange';
+    var $cp = '';
+    var $items_limit = 0;
+    var $stripHTML = False;
+    var $date_format = '';
+
+    // -------------------------------------------------------------------
+    // Private variables
+    // -------------------------------------------------------------------
+    var $channeltags = array ('title', 'link', 'description', 'language', 'copyright', 'managingEditor', 'webMaster', 'lastBuildDate', 'rating', 'docs');
     var $itemtags = array('title', 'link', 'description', 'author', 'category', 'comments', 'enclosure', 'guid', 'pubDate', 'source');
     var $imagetags = array('title', 'url', 'link', 'width', 'height');
     var $textinputtags = array('title', 'description', 'name', 'link');
-
-    var $strip_html = true;
-
-    var $time_out = 5;
-    var $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20040913 Firefox/0.10";
 
     // -------------------------------------------------------------------
     // Parse RSS file and returns associative array.
@@ -76,39 +82,48 @@ class lastRSS {
         // return result
         return $result;
     }
-
+    
     // -------------------------------------------------------------------
-    // Modification of preg_match(); return trimmed field with index 1
+    // Modification of preg_match(); return trimed field with index 1
     // from 'classic' preg_match() array output
     // -------------------------------------------------------------------
     function my_preg_match ($pattern, $subject) {
+        // start regullar expression
         preg_match($pattern, $subject, $out);
-        return trim($out[1]);
+
+        // if there is some result... process it and return it
+        if(isset($out[1])) {
+            // Process CDATA (if present)
+            if ($this->CDATA == 'content') { // Get CDATA content (without CDATA tag)
+                $out[1] = strtr($out[1], array('<![CDATA['=>'', ']]>'=>''));
+            } elseif ($this->CDATA == 'strip') { // Strip CDATA
+                $out[1] = strtr($out[1], array('<![CDATA['=>'', ']]>'=>''));
+            }
+
+            // If code page is set convert character encoding to required
+            if ($this->cp != '')
+                //$out[1] = $this->MyConvertEncoding($this->rsscp, $this->cp, $out[1]);
+                $out[1] = iconv($this->rsscp, $this->cp.'//TRANSLIT', $out[1]);
+            // Return result
+            return trim($out[1]);
+        } else {
+        // if there is NO result, return empty string
+            return '';
+        }
     }
 
     // -------------------------------------------------------------------
     // Replace HTML entities &something; by real characters
     // -------------------------------------------------------------------
     function unhtmlentities ($string) {
-        $trans_tbl = get_html_translation_table (HTML_ENTITIES);
+        // Get HTML entities table
+        $trans_tbl = get_html_translation_table (HTML_ENTITIES, ENT_QUOTES);
+        // Flip keys<==>values
         $trans_tbl = array_flip ($trans_tbl);
+        // Add support for &apos; entity (missing in HTML_ENTITIES)
+        $trans_tbl += array('&apos;' => "'");
+        // Replace entities by values
         return strtr ($string, $trans_tbl);
-    }
-
-    // -------------------------------------------------------------------
-    // Encoding conversion function
-    // -------------------------------------------------------------------
-    function MyConvertEncoding($in_charset, $out_charset, $string) {
-        // if substitute_character
-        if ($this->subs_char) {
-            // Iconv() to UTF-8. mb_convert_encoding() to $out_charset
-            $utf = iconv($in_charset, 'UTF-8', $string);
-            mb_substitute_character($this->subs_char);
-            return mb_convert_encoding ($utf, $out_charset, 'UTF-8');
-        } else {
-            // Iconv() to $out_charset
-            return iconv($in_charset, $out_charset, $string);
-        }
     }
 
     // -------------------------------------------------------------------
@@ -116,73 +131,89 @@ class lastRSS {
     // Don't use Parse() in your scripts - use Get($rss_file) instead.
     // -------------------------------------------------------------------
     function Parse ($rss_url) {
-        include_once( "db.php" );
-        include_once( "functions.php" );
-
         // Open and load RSS file
-        $rss_content = fetchHTML( $rss_url );
-
-        if( empty( $rss_content ) )
-        {
-            return false;
-        }
-
-        // Parse document encoding
-        $result['encoding'] = $this->my_preg_match("'encoding=[\'\"](.*?)[\'\"]'si", $rss_content);
-
-        // If code page is set convert character encoding to required
-            if ($this->cp != '')
-                $rss_content = $this->MyConvertEncoding($result['encoding'], $this->cp, $rss_content);
-
-        // Parse CHANNEL info
-        preg_match("'<channel.*?>(.*?)</channel>'si", $rss_content, $out_channel);
-        foreach($this->channeltags as $channeltag)
-        {
-            $temp = $this->my_preg_match("'<$channeltag.*?>(.*?)</$channeltag>'si", $out_channel[1]);
-            if ($temp != '') $result[$channeltag] = $temp; // Set only if not empty
-
-        }
-
-        // Parse TEXTINPUT info
-        preg_match("'<textinput(|[^>]*[^/])>(.*?)</textinput>'si", $rss_content, $out_textinfo);
-            // This a little strange regexp means:
-            // Look for tag <textinput> with or without any attributes, but skip truncated version <textinput /> (it's not beginning tag)
-        if ($out_textinfo[2]) {
-            foreach($this->textinputtags as $textinputtag) {
-                $temp = $this->my_preg_match("'<$textinputtag.*?>(.*?)</$textinputtag>'si", $out_textinfo[2]);
-                if ($temp != '') $result['textinput_'.$textinputtag] = $temp; // Set only if not empty
+        if ($f = @fopen($rss_url, 'r')) {
+            $rss_content = '';
+            while (!feof($f)) {
+                $rss_content .= fgets($f, 4096);
             }
-        }
-        // Parse IMAGE info
-        preg_match("'<image.*?>(.*?)</image>'si", $rss_content, $out_imageinfo);
-        if ($out_imageinfo[1]) {
-            foreach($this->imagetags as $imagetag) {
-                $temp = $this->my_preg_match("'<$imagetag.*?>(.*?)</$imagetag>'si", $out_imageinfo[1]);
-                if ($temp != '') $result['image_'.$imagetag] = $temp; // Set only if not empty
-            }
-        }
-        // Parse ITEMS
-        preg_match_all("'<item(| .*?)>(.*?)</item>'si", $rss_content, $items);
-        $rss_items = $items[2];
-        $result['items_count'] = count($items[1]);
-        $i = 0;
-        $result['items'] = array(); // create array even if there are no items
-        foreach($rss_items as $rss_item) {
-            // Parse one item
-            foreach($this->itemtags as $itemtag)
+            fclose($f);
+
+            // Parse document encoding
+            $result['encoding'] = $this->my_preg_match("'encoding=[\'\"](.*?)[\'\"]'si", $rss_content);
+            // if document codepage is specified, use it
+            if ($result['encoding'] != '')
+                { $this->rsscp = $result['encoding']; } // This is used in my_preg_match()
+            // otherwise use the default codepage
+            else
+                { $this->rsscp = $this->default_cp; } // This is used in my_preg_match()
+
+            // Parse CHANNEL info
+            preg_match("'<channel.*?>(.*?)</channel>'si", $rss_content, $out_channel);
+            foreach($this->channeltags as $channeltag)
             {
-                $temp = $this->my_preg_match("'<$itemtag.*?>(.*?)</$itemtag>'si", $rss_item);
-                if ($temp != '') $result[items][$i][$itemtag] = $temp; // Set only if not empty
+                $temp = $this->my_preg_match("'<$channeltag.*?>(.*?)</$channeltag>'si", $out_channel[1]);
+                if ($temp != '') $result[$channeltag] = $temp; // Set only if not empty
             }
-            // Strip HTML tags and other bullshit from DESCRIPTION (if description is presented)
-            if ($result['items'][$i]['description'] && $this->strip_html == true)
-            {
-                $result['items'][$i]['description'] = strip_tags($this->unhtmlentities(strip_tags($result['items'][$i]['description'])));
+            // If date_format is specified and lastBuildDate is valid
+            if ($this->date_format != '' && ($timestamp = strtotime($result['lastBuildDate'])) !==-1) {
+                        // convert lastBuildDate to specified date format
+                        $result['lastBuildDate'] = date($this->date_format, $timestamp);
             }
-            // Item counter
-            $i++;
+
+            // Parse TEXTINPUT info
+            preg_match("'<textinput(|[^>]*[^/])>(.*?)</textinput>'si", $rss_content, $out_textinfo);
+                // This a little strange regexp means:
+                // Look for tag <textinput> with or without any attributes, but skip truncated version <textinput /> (it's not beggining tag)
+            if (isset($out_textinfo[2])) {
+                foreach($this->textinputtags as $textinputtag) {
+                    $temp = $this->my_preg_match("'<$textinputtag.*?>(.*?)</$textinputtag>'si", $out_textinfo[2]);
+                    if ($temp != '') $result['textinput_'.$textinputtag] = $temp; // Set only if not empty
+                }
+            }
+            // Parse IMAGE info
+            preg_match("'<image.*?>(.*?)</image>'si", $rss_content, $out_imageinfo);
+            if (isset($out_imageinfo[1])) {
+                foreach($this->imagetags as $imagetag) {
+                    $temp = $this->my_preg_match("'<$imagetag.*?>(.*?)</$imagetag>'si", $out_imageinfo[1]);
+                    if ($temp != '') $result['image_'.$imagetag] = $temp; // Set only if not empty
+                }
+            }
+            // Parse ITEMS
+            preg_match_all("'<item(| .*?)>(.*?)</item>'si", $rss_content, $items);
+            $rss_items = $items[2];
+            $i = 0;
+            $result['items'] = array(); // create array even if there are no items
+            foreach($rss_items as $rss_item) {
+                // If number of items is lower then limit: Parse one item
+                if ($i < $this->items_limit || $this->items_limit == 0) {
+                    foreach($this->itemtags as $itemtag) {
+                        $temp = $this->my_preg_match("'<$itemtag.*?>(.*?)</$itemtag>'si", $rss_item);
+                        if ($temp != '') $result['items'][$i][$itemtag] = $temp; // Set only if not empty
+                    }
+                    // Strip HTML tags and other bullshit from DESCRIPTION
+                    if ($this->stripHTML && $result['items'][$i]['description'])
+                        $result['items'][$i]['description'] = strip_tags($this->unhtmlentities(strip_tags($result['items'][$i]['description'])));
+                    // Strip HTML tags and other bullshit from TITLE
+                    if ($this->stripHTML && $result['items'][$i]['title'])
+                        $result['items'][$i]['title'] = strip_tags($this->unhtmlentities(strip_tags($result['items'][$i]['title'])));
+                    // If date_format is specified and pubDate is valid
+                    if ($this->date_format != '' && ($timestamp = strtotime($result['items'][$i]['pubDate'])) !==-1) {
+                        // convert pubDate to specified date format
+                        $result['items'][$i]['pubDate'] = date($this->date_format, $timestamp);
+                    }
+                    // Item counter
+                    $i++;
+                }
+            }
+
+            $result['items_count'] = $i;
+            return $result;
         }
-        return $result;
+        else // Error in opening return False
+        {
+            return False;
+        }
     }
 }
 
